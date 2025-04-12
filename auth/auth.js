22df5685f24d40fea25cefcdcf4ab6068461a6ea
Fuzer <fuuzer_@hotmail.com>
2025-04-11 21:30:07 -0300

    login backend

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,7 @@
-const button = document.getElementById('botao-cadastrar');  
+const buttonRegister = document.getElementById('botao-cadastrar');  
+const loginButton = document.getElementById('login-btn');  
 
-button.addEventListener('click', (e) => {
+buttonRegister.addEventListener('click', (e) => {
 e.preventDefault()
   const nome = document.getElementById('nome').value;
   const email = document.getElementById('email').value;
@@ -18,5 +19,18 @@ e.preventDefault()
   .then(data => {
     alert(data.message)
   })
-})
+});
 
+loginButton.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  fetch('http://localhost:5000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: 'fuzer@hotmail.com', password: '123456'})
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+})
